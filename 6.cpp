fdefbe2afdb92d0f811998629627b7d7d89fb0f8
Barun Dhiman <barundhiman1223@gmail.com>
2026-08-07 01:09:09 +0530

    some namespaces and std alternatives

---
+++ 6.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+int main(){
+    using namespace std; //also use-> using std::cout or
+      string name="Barun"; //using std::string
+    cout << name <<" hello c world";
+}                   
