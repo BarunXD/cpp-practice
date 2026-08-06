5ab5504fde6dbd3597676f0ca703ac4df48c8644
Barun Dhiman <barundhiman1223@gmail.com>
2026-08-06 23:21:26 +0530

    variables

---
+++ 3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+int main(){
+    //integers(whole numbers)
+    int age=21;
+    int year=2026;
+    int days=7;
+ //double(using decimals)
+    double price=10.99;
+    double gpa=2.5;
+    double temperature=25.1;
+    std::cout << price <<'\n';
+    //char (store only single charachter)
+    char initials='A';
+    char grades='B';
+    std::cout << grades << '\n';
+    //string
+    std::string name="Barun";
+    std::string day="Friday";
+    std::cout<< day<<'\n';
+    std::cout<< name<<'\n';
+
+    std::cout<< "hello "<< name<<'\n';
+    std::cout<< "you are "<< age << "years old.";
+    return 0;
+}
