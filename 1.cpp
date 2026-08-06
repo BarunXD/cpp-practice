aac3e333b90e81af0a4e14cacf349a6a880f56f6
Barun Dhiman <barundhiman1223@gmail.com>
2026-08-06 22:35:00 +0530

    My first C++ program

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-    cout << "Hello C++";
+    //This is my first c++ code 
+    /*I am excited to learn c++ language*/
+    std::cout << "Hello C++" <<std::endl;
+    std::cout << "I like pizza" <<std::endl;
+    std::cout << "Its really good!" <<'\n';
     return 0;
 }
