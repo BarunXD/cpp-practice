a1ea6693fd3d93f37fc7157c518fc75270e502e1
Barun Dhiman <barundhiman1223@gmail.com>
2026-08-07 14:38:35 +0530

    swap

--- q3.cpp
+++ q3.cpp
@@ -1,4 +1,14 @@
 #include<iostream>
 int main(){
-    
+    int a=10;
+    int b=5;
+    int c;
+    c=a;
+    a=b;
+    b=c;
+    std::cout<<"value of a "<<a<<'\n';
+    std::cout<<"value of b is "<<b<<'\n';
+    std::swap(a,b);
+    std::cout<<"a is "<<a<<'\n';
+    std::cout<<"b is "<<b;
 }
